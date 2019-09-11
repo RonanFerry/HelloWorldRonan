0df93e26106ca65aed2970c12e8a2c773a810603
Ronan Ferry <Ronan.Ferry@students.ittralee.ie>
2019-09-11 10:44:38 +0100

    Initial commit

---
+++ src/HelloWorld2.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class HelloWorld2 {
+            //This is a comment
+    public static void main(String[] args) {
+
+        String name = JOptionPane.showInputDialog("Enter Name");
+
+        JOptionPane.showMessageDialog(null, name);
+
+    }
+}
